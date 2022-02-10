e6295e29c5043a2162b341c521b0f4ca96dfbdbb
Diego Gonzalez <gonzalezdiego1990@gmail.com>
2022-02-10 16:28:34 -0300

    Feat: add records to categories table

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -13,6 +15,37 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+            [
+                'name' => 'Celulares y tablets',
+                'slug' => Str::slug('Celulares y tablets'),
+                'icon' => '<i class="fa-solid fa-mobile-screen-button"></i>',
+            ],
+            [
+                'name' => 'TV, audio y video',
+                'slug' => Str::slug('TV, audio y video'),
+                'icon' => '<i class="fa-solid fa-tv"></i>',
+            ],
+            [
+                'name' => 'Consola y videojuegos',
+                'slug' => Str::slug('Consola y videojuegos'),
+                'icon' => '<i class="fa-solid fa-gamepad"></i>',
+            ],
+            [
+                'name' => 'Computación',
+                'slug' => Str::slug('Computación'),
+                'icon' => '<i class="fa-solid fa-laptop"></i>',
+            ],
+            [
+                'name' => 'Moda',
+                'slug' => Str::slug('Moda'),
+                'icon' => '<i class="fa-solid fa-shirt"></i>',
+            ],
+
+        ];
+
+        foreach ($categories as $category) {
+            Category::factory(1)->create($category);
+        }
     }
 }
